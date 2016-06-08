2771a2c89d74ecda25c1f5d6da15268a2685814b
Alberto <aldeluis@gmail.com>
2016-06-09 01:28:29 +0200

    Update modelo.js

--- modelo.js
+++ modelo.js
@@ -1,49 +1,51 @@
 var db = require('knex')({
-	debug: true,
-  client: 'postgresql',
-  connection: {
-    host     : '127.0.0.1',
-    user     : 'knex',
-    password : 'knex',
-    database : 'knex',
+  debug: true,
+  client: 'pg',
+  connection:{host:"localhost",user:"test",password:"test",database:"test"},
   searchPath: 'public',
-  }
 });
 
-//var sql = require('knex')({client:'pg'});
 
-//console.log(db('table').select().returning('*').toString());
-//console.log(
-//sql.schema.createTableIfNotExists('personas2',function(t){
-//	t.increments('uid').primary();
-//}).toString()
-//);
+// This drops and creates table every time
+db.schema.dropTableIfExists('todos').then(function(rows){
+  return db.schema.createTable('todos', function(table) {
+    table.increments('id');
+    table.string('text');
+    table.boolean('complete');
+  })}).return();
 
-db.schema.createTableIfNotExists('PERSONAS',function(table){
-	table.increments('id_persona').primary();
-	table.string('denominacion');
-	table.string('numero_docid');
-	table.enum('tipo_docid',['DNI','NIF','CIF','pasaporte']);
-	table.enum('tipo_pers',['persona','entidad','grupo']);
-	table.date('f_inicio');
-	table.date('f_final');
+db.schema.createTableIfNotExists('Modulos',function(table){
+  table.increments('id').primary();
+  table.string('nombre'); // ficha que hace referencia
+  table.string('ficha'); // modelo de la DB hace referencia
 }).return();
 
 
-db.schema.createTableIfNotExists('PERSxPERS',function(table){
-	table.increments('id_persXpers').primary();
-	table.integer('id_persona_A').references('uid').inTable('personas');
-	table.integer('id_persona_B').references('uid').inTable('personas');
-	table.enum('tipo_relacion',['contrato','plaza','adscripción','cargo']);
-	table.string('relacion');
-	table.date('f_inicio');
-	table.date('f_final');
+db.schema.createTableIfNotExists('Campos',function(table){
+  table.increments('id').primary();
+  table.string('ficha'); // ficha que hace referencia
+  table.string('model'); // modelo de la DB hace referencia
+  table.string('orden'); // orden en el que aparece
+  table.string('posic'); // posicion dentro de la view
+  table.string('campo'); // campo de la base de datos 
+  table.string('etiqu'); // label
+  table.string('clase'); // tipo de input del formulario
 }).return();
 
-db.schema.createTableIfNotExists('PERSoLOCALIZADORES',function(table){
-	table.increments('id_localizador').primary();
-	table.integer('id_persona').references('uid').inTable('personas');
-	table.enum('tipo_localizador',['teléfono','email','fax','url-www','linkedin']);
-	table.string('localizador');
-	table.string('observaciones');
+
+db.schema.createTableIfNotExists('Personas',function(table){
+  table.increments('id').primary();
+  table.string('nombre');
+  table.string('apellidos');
+  table.string('nip');
+  table.enum('t_nip',['DNI','Pasaporte', 'NIE']);
+  table.enum('sexo',['0', '1']);
+  table.date('f_inicio');
+  table.date('f_final');
+  table.date('f_modif');
+  table.date('f_creac');
+  table.date('f_vigen');
+  table.date('u_modif_id').references('id').inTable('Personas');
 }).return();
+
+module.exports = db;
