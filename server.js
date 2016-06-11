14226622c0c331565208bd889c8f315a208aad89
aldeluis <alberto@deluis.es>
2016-06-11 09:42:04 -0400

    actualización a feathers 2

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var feathers = require('feathers');
 var rest = require('feathers-rest');
 var bodyParser = require('body-parser');
 var service = require('feathers-knex');
+var socketio = require('feathers-socketio');
 var db = require('./modelo.js')
 
 var app = feathers()
   .use(feathers.static(__dirname+'/public'))
   .configure(rest())
-  .configure(feathers.socketio())
-  .configure(feathers.rest())
+  .configure(socketio())
+  .configure(rest())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({extended: true}))
 
