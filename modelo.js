8f95238fdab9b379ca1bea5a38bc58e855cab6ca
Al <code@deluis.es>
2016-06-14 03:19:59 +0200

    las tablas se crean si no existen y se rellenan con datos de prueba si están vacias

--- modelo.js
+++ modelo.js
@@ -1,26 +1,34 @@
 var db = require('knex')({
-  debug: true,
   client: 'pg',
   connection:{host:"localhost",user:"test",password:"test",database:"test"},
-  searchPath: 'public',
+  searchPath: 'public'
 });
 
+// Sólo crea la tabla si no existe
+db.schema.createTableIfNotExists('todos',function(table) {
+  table.increments('id');
+  table.string('text');
+  table.boolean('complete');
+}).return();
 
-// This drops and creates table every time
-db.schema.dropTableIfExists('todos').then(function(rows){
-  return db.schema.createTable('todos', function(table) {
-    table.increments('id');
-    table.string('text');
-    table.boolean('complete');
-  })}).return();
-
+// Sólo crea la tabla si no existe
 db.schema.createTableIfNotExists('Modulos',function(table){
   table.increments('id').primary();
   table.string('nombre'); // ficha que hace referencia
   table.string('ficha'); // modelo de la DB hace referencia
+  table.string('model'); // modelo de la DB hace referencia
 }).return();
 
+// Sólo inserta datos si no hay datos
+db("Modulos").select().count().then(function(r){
+  if (r[0].count==0) {return db("Modulos").insert([
+    {nombre:"Ficha de Fichas", ficha:"fichaFichas",    model:"Campos"},
+    {nombre:"Personas",        ficha:"fichaPersonas",  model:"Personas"},
+    {nombre:"Entidades",       ficha:"fichaEntidades", model:"Entidades"},
+    {nombre:"Eventos",         ficha:"fichaEventos",   model:"Eventos"}
+  ])}});
 
+// Sólo crea la tabla si no existe
 db.schema.createTableIfNotExists('Campos',function(table){
   table.increments('id').primary();
   table.string('ficha'); // ficha que hace referencia
@@ -30,9 +38,27 @@ db.schema.createTableIfNotExists('Campos',function(table){
   table.string('campo'); // campo de la base de datos 
   table.string('etiqu'); // label
   table.string('clase'); // tipo de input del formulario
+  table.boolean('enlista'); // tipo de input del formulario
 }).return();
 
+// Sólo inserta datos si no hay datos
+db("Campos").select().count().then(function(r){
+  if (r[0].count==0) {return db("Campos").insert([
+    {ficha:'fichaFichas',   model:'Campos',   orden:'01', posic:'m12', campo:'id',         etiqu:'ID',       clase:'caja',     enlista:0},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'02', posic:'m12', campo:'ficha',      etiqu:'Ficha',    clase:'caja',     enlista:1},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'03', posic:'m12', campo:'model',      etiqu:'Modelo',   clase:'caja',     enlista:0},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'04', posic:'m12', campo:'orden',      etiqu:'Orden',    clase:'caja',     enlista:0},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'05', posic:'m12', campo:'posic',      etiqu:'Posición', clase:'caja',     enlista:0},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'06', posic:'m12', campo:'campo',      etiqu:'Campo',    clase:'caja',     enlista:0},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'07', posic:'m12', campo:'etiqu',      etiqu:'Etiqueta', clase:'caja',     enlista:0},
+    {ficha:'fichaFichas',   model:'Campos',   orden:'08', posic:'m12', campo:'clase',      etiqu:'Clase',    clase:'combobox', enlista:0},
+    {ficha:'fichaPersonas', model:'Personas', orden:'01', posic:'m12', campo:'nombre',     etiqu:'Nombre',   clase:'cajaLista',enlista:1},
+    {ficha:'fichaPersonas', model:'Personas', orden:'02', posic:'m12', campo:'apellidos',  etiqu:'Apellidos',clase:'caja',     enlista:1},
+    {ficha:'fichaPersonas', model:'Personas', orden:'03', posic:'m12', campo:'nip',        etiqu:'NIP',      clase:'caja',     enlista:0}
+  ])}});
 
+
+// Sólo crea la tabla si no existe
 db.schema.createTableIfNotExists('Personas',function(table){
   table.increments('id').primary();
   table.string('nombre');
@@ -46,6 +72,13 @@ db.schema.createTableIfNotExists('Personas',function(table){
   table.date('f_creac');
   table.date('f_vigen');
   table.date('u_modif_id').references('id').inTable('Personas');
-}).return();
+});
+
+// Sólo inserta datos si no hay datos
+db("Personas").select().count().then(function(r){
+  if (r[0].count==0) {return db("Personas").insert([
+    {nombre:'Adonay', apellidos:'Sanz'},
+    {nombre:'Bruno',  apellidos:'Maltrás'}
+  ])}});
 
 module.exports = db;
