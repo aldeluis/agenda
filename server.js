33da38f5d4fb8fc3cb4bc5ce51722316f2cdd52b
Al <code@deluis.es>
2016-06-24 01:09:56 +0200

    Uso de hooks con timestaps y ejemplos

--- server.js
+++ server.js
@@ -1,18 +1,85 @@
-var feathers = require('feathers');
-var handler = require('feathers-errors/handler');
-var socketio = require('feathers-socketio');
-var service = require('feathers-knex');
-var db = require('./modelo.js')
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const handler = require('feathers-errors/handler');
+const socketio = require('feathers-socketio');
+const service = require('feathers-knex');
+const db = require('./modelo.js');
+const util = require('util');
+const hooks = require('feathers-hooks');
 
 // Create a feathers instance.
 var app = feathers()
+  // Habilitamos la REST API
+  .configure(rest())
   // Setup the public folder
   .use(feathers.static(__dirname+'/public'))
   // Enable Socket.io
   .configure(socketio())
+  .configure(hooks())
 
 // Connect to the db, create and register a Feathers service.
 app.use('/todos', service({Model: db, name: 'todos'}));
+app.use('/campos', service({Model: db, name: 'campos'}));
+
+//// Hook con desactivación de trasporte
+//app.service('vista').before({
+//  all: hooks.disable('socketio')
+//});
+
+// Hook con desactivación de método en un transporte
+//app.service('vista').before({
+//  get: hooks.disable()
+//});
+
+// Hook con log
+//app.service('vista').before({
+//  find:[console.log("hook!")]
+//});
+
+// Hook con log externo
+//let mihook = function (options) {
+//  return function(hook) {
+//    console.log(util.inspect(hook))
+//  }
+//}
+//
+//app.service('vista').before({
+//  find: [mihook()]
+//})
+
+// Hook con array de acciones
+//app.service('vista').before({
+//  find: function(){console.log("hola")}
+//});
+
+// Hook modificando el query 
+//app.service('campos').before({
+//  all: hooks.pluckQuery('model'),
+//  get: [function(hook){
+//    hook.params.query={model:"Todos"}
+//  },
+//  function(hook){console.log(hook.query)}]
+//})
+
+// Hooks before
+app.service('todos').before({
+  create(hook) {
+    hook.data.created = new Date();
+  },
+  patch(hook) {
+    hook.data.updated = new Date();
+  }
+});
+
+
+// Hooks after
+//app.service('campos').after({
+//  find: function(hook) {
+//    hook.result.forEach(function(row){
+//      console.log(row.model);
+//    });
+//  }
+//});
 
 // Start the server
 var port = 80;
